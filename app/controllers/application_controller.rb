9047454426ed0faffa175c04270dcb7b17876ab1
the-alchemist-gh <malikbanks01@gmail.com>
2022-09-13 23:40:15 +0000

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
     { message: "Hello world" }.to_json
   end
 
+  get "/games" do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get "/games/:id" do
+    games = Game.find(params[:id])
+    games.to_json(include: {reviews:{include: :user}})
+  end
 end
